28ce7c735d868e7914e24892f15218a9f8792cc2
AlexeiKransiski <alexeikran727@yahoo.com>
2016-06-09 05:44:14 +0400

    gg

--- alerter.go
+++ alerter.go
@@ -19,9 +19,11 @@ func NewAlerter(server, password string) (*Alerter, error) {
 			MaxIdle:     3,
 			IdleTimeout: 240 * time.Second,
 			Dial: func() (redis.Conn, error) {
+
 				c, err := redis.Dial("tcp", server)
 				if err != nil {
 					return nil, err
+					
 				}
 				if password != "" {
 					if _, err := c.Do("AUTH", password); err != nil {
